9525644090e9c597b524aac844d78365dd6e34b6
siddhartha <sidhm12@gmail.com>
2017-04-02 17:11:08 -0700

    Initial Commit

---
+++ src/main/java/com/mvs/learn/serivces/LearnServicesApplication.java
@@ -0,0 +1,12 @@
+package com.mvs.learn.serivces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mvs/learn/serivces/LearnServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mvs.learn.serivces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
